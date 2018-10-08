711246d06729f30684693a8035d3eb39d97c7716
Leonski <leonczukdavid@gmail.com>
2018-10-08 20:12:43 +0200

    change

--- Day2/Zadanie14.py
+++ Day2/Zadanie14.py
@@ -1,12 +1,12 @@
 s = ()
-z
+z=[]
 ilość = 0
 print("Możesz zakończyć wprowadzanie liczb pisząc STOP.")
-while s == "STOP":
+while s != ("STOP"):
     x = input("Podaj liczbę: ")
+    z.append(x)
     if x == ():
         print("Nie została podana żadna liczba.")
     else:
         ilość+1
-    z.append(x)
 print (z)
